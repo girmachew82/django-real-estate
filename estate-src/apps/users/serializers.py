8c3f07dacf8b6567327665cc47168b5c0b0a37d0
girmachew82 <girmachewabebe@gmail.com>
2025-05-31 12:54:21 +0300

    profile agents top agents

--- estate-src/apps/users/serializers.py
+++ estate-src/apps/users/serializers.py
@@ -11,13 +11,13 @@ User = get_user_model()
 class UserSerializer(serializers.ModelSerializer):
     sex = serializers.CharField(source="profile.sex")
     phone_number = PhoneNumberField(source="profile.phone_number")
-    profile_photo = serializers.ImageField(source="profile.phone_number")
-    country = CountryField(source="profile.coutry")
+    profile_photo = serializers.ImageField(source="profile.photo")
+    country = CountryField(source="profile.country")
     city = serializers.CharField(source="profile.city")
     top_seller = serializers.BooleanField(source="profile.top_seller")
     first_name = serializers.SerializerMethodField()
     last_name = serializers.SerializerMethodField()        
-    full_name = serializers.SerializerMethodField(source="get_full_name")    
+    full_name = serializers.SerializerMethodField()    
 
     class Meta:
         model=User
@@ -26,10 +26,12 @@ class UserSerializer(serializers.ModelSerializer):
     def get_first_name(self, obj):
         return obj.first_name.title()
     def get_last_name(self, obj):
-        return obj.first_name.title()  
+        return obj.last_name.title() 
+    def get_full_name(self, obj):
+        return obj.get_full_name
     def to_representation(self, instance):
-        representation = super(UserSerializer, self).to_representation
-        (instance)
+        representation = super(UserSerializer, self).to_representation(instance)
+    
         if instance.is_superuser:
             representation['admin'] = True
         return representation
